e4b65004eb51bbab69545310e9717dbd5d971e97
yupthatpandadk <yupthatpanda@gmail.com>
2026-09-14 17:49:07 +0200

    Add police identity, search, fine and plate lookup configuration

--- config.lua
+++ config.lua
@@ -4,6 +4,8 @@ Config.JobName = 'police'
 Config.InteractionDistance = 3.0
 Config.RequireDuty = true
 Config.DefaultCallsign = '2-00'
+Config.MaxFine = 25000
+Config.Inventory = 'auto' -- auto, ox_inventory, esx
 
 Config.Commands = {
     duty = 'pduty',
@@ -11,7 +13,11 @@ Config.Commands = {
     cuff = 'cuff',
     escort = 'escort',
     vehicle = 'putincar',
-    removeVehicle = 'outcar'
+    removeVehicle = 'outcar',
+    id = 'checkid',
+    search = 'search',
+    fine = 'fine',
+    plate = 'plate'
 }
 
 Config.Fines = {
